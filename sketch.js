7fa9f8a61c12ca7a4e73d0d3220177f813e27dbc
B0tta <90757307+B0tta@users.noreply.github.com>
2021-09-29 10:01:06 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,32 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  createCanvas((windowWidth * 3) / 2, windowHeight);
+  angleMode(DEGREES);
+  background("#2b2b2b");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noFill();
+
+  let myColor = lerpColor(color("#03045E"), color("#CAF0F8"), frameCount / 120);
+
+  stroke(myColor);
+  translate(0, frameCount * 3);
+  rect(100, 100, 150);
+  rect(550, 100, 150);
+  rect(793, 100, 150);
+  rect(1000, 100, 150);
+  rect(1450, 100, 150);
+  rect(1693, 100, 150);
+
+  translate(frameCount * 2, -frameCount * 3);
+  rect(100, 463, 150);
+  rect(550, 463, 150);
+  rect(1000, 100, 150);
+  rect(1000, 463, 150);
+  rect(1450, 100, 150);
+  rect(1450, 300, 150);
+
+  if (frameCount > 120) {
+    noLoop();
+  }
 }
